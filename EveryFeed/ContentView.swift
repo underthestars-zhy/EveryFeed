00f2ce05e63fe4198334aaee33055230e76715ec
underthestars-zhy <zhuhaoyu0909@icloud.com>
2021-02-16 20:05:43 +0800

    Initial Commit

---
+++ EveryFeed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EveryFeed
+//
+//  Created by 朱浩宇 on 2021/2/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EveryFeed/EveryFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveryFeedApp.swift
+//  EveryFeed
+//
+//  Created by 朱浩宇 on 2021/2/16.
+//
+
+import SwiftUI
+
+@main
+struct EveryFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
